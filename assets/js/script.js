95e79297f296446643b16c8e57c6a67a3a5df3c2
Reem Ahmed <reem.mda0909@gmail.com>
2022-03-19 12:04:34 -0400

    add save events

--- assets/js/script.js
+++ assets/js/script.js
@@ -180,6 +180,118 @@ else if (moment().isAfter(hour18)) {
 else {
     $("#timeblock17").addClass("future");
 };
-//End Audit Timeblock
+
+//Audit time block Ends
+
+//Add delete event function for each time block
+$("#save-delete6").on("click", "i.fa-trash", function () {
+    localStorage.removeItem("hour6");
+    $("eventblock6").val("");
+})
+$("#save-delete7").on("click" , "i.fa-trash", function () {
+    localStorage.removeItem("hour7");
+    $("#eventblock7").val("");
+})
+$("#save-delete8").on("click" , "i.fa-trash", function () {
+    localStorage.removeItem("hour8");
+    $("#eventblock8").val("");
+})
+$("#save-delete9").on("click" , "i.fa-trash", function () {
+    localStorage.removeItem("hour9");
+    $("#eventblock9").val("");
+})
+$("#save-delete10").on("click" , "i.fa-trash", function () {
+    localStorage.removeItem("hour10");
+    $("#eventblock10").val("");
+})
+$("#save-delete11").on("click" , "i.fa-trash", function () {
+    localStorage.removeItem("hour11");
+    $("#eventblock11").val("");
+})
+$("#save-delete12").on("click" , "i.fa-trash", function () {
+    localStorage.removeItem("hour12");
+    $("#eventblock12").val("");
+})
+$("#save-delete13").on("click" , "i.fa-trash", function () {
+    localStorage.removeItem("hour13");
+    $("#eventblock13").val("");
+})
+$("#save-delete14").on("click" , "i.fa-trash", function () {
+    localStorage.removeItem("hour14");
+    $("#eventblock14").val("");
+})
+$("#save-delete15").on("click" , "i.fa-trash", function () {
+    localStorage.removeItem("hour15");
+    $("#eventblock15").val("");
+})
+$("#save-delete16").on("click" , "i.fa-trash", function () {
+    localStorage.removeItem("hour16");
+    $("#eventblock16").val("");
+})
+$("#save-delete17").on("click" , "i.fa-trash", function () {
+    localStorage.removeItem("hour17");
+    $("#eventblock17").val("");
+})
+//End of delete event functions
+
+
+//Add save event function for each time block
+$("#save-delete6").on("click", "i.fa-save", function () {
+    var event6 = $("#eventblock6").val().trim();
+    localStorage.setItem('hour6', JSON.stringify(event6));
+})
+$("#save-delete7").on("click", "i.fa-save", function () {
+    var event7 = $("#eventblock7").val().trim();
+    localStorage.setItem('hour7', JSON.stringify(event7));
+})
+$("#save-delete8").on("click", "i.fa-save", function () {
+    var event8 = $("#eventblock8").val().trim();
+    localStorage.setItem('hour8', JSON.stringify(event8));
+})
+$("#save-delete9").on("click", "i.fa-save", function () {
+    var event9 = $("#eventblock9").val().trim();
+    localStorage.setItem('hour9', JSON.stringify(event9));
+})
+$("#save-delete10").on("click", "i.fa-save", function () {
+    var event10 = $("#eventblock10").val().trim();
+    localStorage.setItem('hour10', JSON.stringify(event10));
+})
+$("#save-delete11").on("click", "i.fa-save", function () {
+    var event11 = $("#eventblock11").val().trim();
+    localStorage.setItem('hour11', JSON.stringify(event11));
+})
+$("#save-delete12").on("click", "i.fa-save", function () {
+    var event12 = $("#eventblock12").val().trim();
+    localStorage.setItem('hour12', JSON.stringify(event12));
+})
+$("#save-delete13").on("click", "i.fa-save", function () {
+    var event13 = $("#eventblock13").val().trim();
+    localStorage.setItem('hour13', JSON.stringify(event13));
+})
+$("#save-delete14").on("click", "i.fa-save", function () {
+    var event14 = $("#eventblock14").val().trim();
+    localStorage.setItem('hour14', JSON.stringify(event14));
+})
+$("#save-delete15").on("click", "i.fa-save", function () {
+    var event15 = $("#eventblock15").val().trim();
+    localStorage.setItem('hour15', JSON.stringify(event15));
+})
+$("#save-delete16").on("click", "i.fa-save", function () {
+    var event16 = $("#eventblock16").val().trim();
+    localStorage.setItem('hour16', JSON.stringify(event16));
+})
+$("#save-delete17").on("click", "i.fa-save", function () {
+    var event17 = $("#eventblock17").val().trim();
+    localStorage.setItem('hour17', JSON.stringify(event17));
+})
+// End save event functions
+
+//This function runs but the time is never refreshed?
+setInterval(function () {
+    $(".event-group .events").each?(function (index, el) {
+        auditTime(el);
+        
+    })
+})
 }
 auditTime();
